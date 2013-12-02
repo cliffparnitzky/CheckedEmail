0d49cb41a182bd25065cee0b713a2ec3e77be2c3
Cliff Parnitzky <github@cliff-parnitzky.de>
2013-12-02 21:33:30 +0100

    Added autoload files.

---
+++ system/modules/CheckedEmail/config/autoload.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package CheckedEmail
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'FormCheckedEmail' => 'system/modules/CheckedEmail/FormCheckedEmail.php',
+));
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'form_checkedEmail' => 'system/modules/CheckedEmail/templates',
+));
